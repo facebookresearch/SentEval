"fix .item() and softmax dim"

senteval/tools/relatedness.py
@@ -44,8 +44,8 @@ class RelatednessPytorch(object):
 
         self.model = nn.Sequential(
             nn.Linear(self.inputdim, self.nclasses),
-            nn.Softmax(),
-            )
+            nn.Softmax(dim=-1),
+        )
         self.loss_fn = nn.MSELoss()
 
         if torch.cuda.is_available():
@@ -108,11 +108,11 @@ class RelatednessPytorch(object):
                 # forward
                 idx = torch.from_numpy(permutation[i:i + self.batch_size]).long().cuda()
                 Xbatch = X[idx]
-                ybatch =y[idx]
+                ybatch = y[idx]
                 output = self.model(Xbatch)
                 # loss
                 loss = self.loss_fn(output, ybatch)
-                all_costs.append(loss.data[0])
+                all_costs.append(loss.item())
                 # backward
                 self.optimizer.zero_grad()
                 loss.backward()
@@ -130,5 +130,5 @@ class RelatednessPytorch(object):
                     probas = self.model(Xbatch).data.cpu().numpy()
                 else:
                     probas = np.concatenate((probas,
-                        self.model(Xbatch).data.cpu().numpy()), axis=0)
+                                             self.model(Xbatch).data.cpu().numpy()), axis=0)
         return probas