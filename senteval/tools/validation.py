580a6977f70631d931c417ff6ef34d4ed3250bbf
Alexis Conneau <aconneau@fb.com>
2018-10-04 07:22:50 -0700

    Fixed bug in validation.py

--- senteval/tools/validation.py
+++ senteval/tools/validation.py
@@ -205,7 +205,7 @@ class SplitClassifier(object):
         regs = [10**t for t in range(-5, -1)] if self.usepytorch else \
                [2**t for t in range(-2, 4, 1)]
         if self.noreg:
-            regs = [1e-9 is self.usepytorch else 1e9]
+            regs = [1e-9 if self.usepytorch else 1e9]
         scores = []
         for reg in regs:
             if self.usepytorch:
