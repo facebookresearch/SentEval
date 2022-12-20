a82a3274d2247a19cf0eafd8f565cefa4cf44c5a
Michael Fatemi <myfatemi04@gmail.com>
2022-12-20 10:27:00 -0500

    replace inspect.getargspec with inspect.signature
    Compatible with new Torch APIs

--- senteval/utils.py
+++ senteval/utils.py
@@ -86,7 +86,7 @@ def get_optimizer(s):
         raise Exception('Unknown optimization method: "%s"' % method)
 
     # check that we give good parameters to the optimizer
-    expected_args = inspect.getargspec(optim_fn.__init__)[0]
+    parameters = list(inspect.signature(optim_fn.__init__).parameters)
     assert expected_args[:2] == ['self', 'params']
     if not all(k in expected_args[2:] for k in optim_params.keys()):
         raise Exception('Unexpected parameters: expected "%s", got "%s"' % (
